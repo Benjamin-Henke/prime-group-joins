4e7d8d4d3031756e50489bc6791379e3e9105380
Benjamin Henke <benjaminmhenke@gmail.com>
2021-08-24 14:11:01 -0500

    Completed 7

--- joins-solution.sql
+++ joins-solution.sql
@@ -46,8 +46,14 @@ GROUP BY "line_items"."quantity";
 
 
 -- 6. How many customers do we have?
+SELECT
+	count("customers"."id")
+FROM "customers";
 
 -- 7. How many products do we carry?
+SELECT
+	count("products"."id")
+FROM "products";
 
 -- 8. What is the total available on-hand quantity of diet pepsi?
 
