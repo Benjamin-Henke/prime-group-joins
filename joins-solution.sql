27b93ba1bab5de3397803f6e13b1ac5bf5df0a7f
Benjamin Henke <benjaminmhenke@gmail.com>
2021-08-24 13:52:55 -0500

    Modified 3 & 4

--- joins-solution.sql
+++ joins-solution.sql
@@ -12,32 +12,40 @@ JOIN "products"
 
 -- 3. Which warehouses have cheetos?
 SELECT
-	"warehouse"."warehouse" AS "warehouseName",
-	count("products"."description") AS "productCount"
+	"warehouse"."warehouse" AS "warehouseName"
 FROM "warehouse"
 JOIN "warehouse_product"
 	ON "warehouse"."id" = "warehouse_product"."warehouse_id"
 JOIN "products"
 	ON "warehouse_product"."product_id" = "products"."id"
-GROUP BY "warehouseName";
-    -- Stuck on what to write to show warehouse name and product description
+GROUP BY "warehouseName" 
+WHERE "products"."id" = '5';
 
 -- 4. Which warehouses have diet pepsi?
-	"products"."description" AS "productName",
-	"warehouse"."warehouse" AS "warehouseName",
-	count("warehouse"."id") AS "warehouseId"
+SELECT
+	"warehouse"."warehouse" AS "warehouseName"
 FROM "warehouse"
 JOIN "warehouse_product"
 	ON "warehouse"."id" = "warehouse_product"."warehouse_id"
 JOIN "products"
 	ON "warehouse_product"."product_id" = "products"."id"
-GROUP BY "warehouseName";
-    -- Stuck on what to write to show warehouse name and product description
+GROUP BY "warehouseName"
+WHERE "products"."id" = '6';
 
 -- 5. Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
+SELECT * FROM "customers"
+JOIN "addresses"
+	ON "customers"."id" = "addresses"."customer_id"
+JOIN "orders"
+	ON "addresses"."id" = "orders"."address_id";
+
 -- 6. How many customers do we have?
+
 -- 7. How many products do we carry?
+
 -- 8. What is the total available on-hand quantity of diet pepsi?
+
+
 --Stretch
 -- How much was the total cost for each order?
 -- How much has each customer spent in total?
